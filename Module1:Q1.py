5bd37a2d9bc8299f89351bb66a7a2a8118b9ae85
dhatrinaithani <149319110+dhatrinaithani@users.noreply.github.com>
2025-08-19 21:15:46 +0530

    Add files via upload

---
+++ Module1:Q1.py
@@ -0,0 +1,10 @@
+a=int(input('enter a number'))
+b=int(input('enter a number'))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("addition is",add)
+print("subtraction is",sub)
+print("multiplication is",mul)
+print("division is",div)
